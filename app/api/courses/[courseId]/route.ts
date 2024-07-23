0a948e27a19e913b31208d9226bddb428cfaafb6
Kanwal Arora <kanwal.arora@clinuvel.com>
2024-07-23 10:06:34 +0100

    Course Title Update

---
+++ app/api/courses/[courseId]/route.ts
@@ -0,0 +1,32 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+    req: Request,
+    { params }: { params: { courseId: string } }
+) {
+    try {
+        const { userId } = auth();
+        const { courseId } = params
+        const values = await req.json();
+
+        if (!userId) {
+            return new Response("Unauthorized", { status: 401 });
+        }
+
+        const course = await db.course.update({
+            where: {
+                id: courseId,
+                userId,
+            },
+            data: {
+                ...values,
+            }
+        });
+        return NextResponse.json(course);
+    } catch (error) {
+        console.error("[COURSE_ID]",error);
+        return new Response("Internal Error", { status: 500 });
+    }
+}
