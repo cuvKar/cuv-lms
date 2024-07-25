5dcd9d2b2fd40952fa0e9d80742c924e470cbebf
Kanwal Arora <kanwal.arora@clinuvel.com>
2024-07-25 12:23:39 +0100

    Deleting Attachments

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
@@ -6,7 +6,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { Button } from '@/components/ui/button' 
 import { useState } from 'react';
-import { ImageIcon, Pencil, PlusCircle, PlusCircleIcon } from 'lucide-react';
+import { File, ImageIcon, Loader2, Pencil, PlusCircle, PlusCircleIcon, X } from 'lucide-react';
 import toast from 'react-hot-toast';
 import { useRouter } from 'next/navigation';
 import { Attachment, Course } from '@prisma/client';
@@ -27,6 +27,7 @@ export const AttachmentForm = ({
     courseId
 }: AttachmentFormProps) => {
     const [isEditing, setIsEditing] = useState(false);
+    const [deletingId, setDeletingId] = useState<string | null>(null);
     const  form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
 })
@@ -42,6 +43,19 @@ const onSubmit = async (values: z.infer<typeof formSchema>) => {
         router.refresh()
     } catch {
         toast. error('Somthing went wrong')
+    }
+}
+
+const onDelete = async (id: string) => {
+    try{
+        setDeletingId(id);
+        await axios.delete(`/api/courses/${courseId}/attachments/${id}`);
+        toast.success('Attachment deleted');
+        router.refresh();
+    } catch {
+        toast.error('Something went wrong')
+    } finally {
+        setDeletingId(null)
     }
 }
   return (
@@ -68,12 +82,40 @@ const onSubmit = async (values: z.infer<typeof formSchema>) => {
                         No attachments yet
                     </p>
                 )}
+
+                {initialData.attachments.length > 0 && (
+                    <div className='space-y-2'>
+                        {initialData.attachments.map((attachment) => (
+                            <div
+                                key={attachment.id}
+                                className='flex items-center p-3 w-full border rounded-md'>
+                                <File className='h-6 w-6 mr-2' />
+                                <p className='text-xs line-clamp-1'>
+                                    {attachment.name}
+                                </p>
+                                {deletingId === attachment.id && (
+                                    <div>
+                                        <Loader2 className='h-6 w-6 animate-spin' />
+                                    </div>
+                                )}
+                                {deletingId !== attachment.id && (
+                                    <button
+                                        onClick={() => onDelete(attachment.id)}
+                                        className='ml-auto hover:opacity-75 transition'
+                                    >
+                                        <X className='h-4 w-4'/>
+                                    </button>
+                                )}
+                            </div>
+                        ))}
+                    </div>
+                )}
             </>
         )}
         {isEditing && (
             <div>
                 <FileUpload
-                    endpoint='courseImage'
+                    endpoint='courseAttachment'
                     onChange={(url) => {
                         if(url){
                             onSubmit({url: url});
