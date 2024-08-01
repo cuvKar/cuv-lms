17398b5ef2860156b44a0db50554355ad34f156f
Kanwal Arora <kanwal.arora@clinuvel.com>
2024-08-01 18:15:11 +0100

    Data tables Component & Sorting

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,15 +1,31 @@
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
 import React from 'react'
+import { DataTable } from './_components/data-table'
+import { columns } from './_components/columns'
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+import { db } from '@/lib/db'
 
-function CoursePage() {
+
+
+const CoursePage = async() => {
+  const { userId } = auth();
+
+  if(!userId) {
+    return redirect('/')
+  }
+
+  const courses = await db.course.findMany({
+    where:{
+      userId: userId
+    },
+    orderBy:{
+      createdAt: 'desc'
+    }
+  })
+
   return (
     <div>
-      <Link href='/teacher/create'>
-        <Button>
-          New Course
-        </Button>
-      </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
   )
 }
