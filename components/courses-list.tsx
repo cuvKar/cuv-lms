6938ded15018df0e979d18805a63fc5af011dd1f
Kanwal Arora <kanwal.arora@clinuvel.com>
2024-08-06 12:01:10 +0100

    Course Card and Course List

--- components/courses-list.tsx
+++ components/courses-list.tsx
@@ -1,4 +1,5 @@
 import { Category, Course } from "@prisma/client";
+import { CourseCard } from "@/components/course-card";
 
 type CourseWithProgressWithCategory = Course & {
     category: Category | null;
@@ -15,11 +16,21 @@ export const CoursesList = ({
 }: CoursesListProps) => {
     return (
         <div>
-            <div className="py-6">
+            <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4 py-6">
                 {items.map((item) => (
-                    <div key={item.id}>
-                        {item.title}
-                    </div>
+                    // <div key={item.id}>
+                    //     {item.title}
+                    // </div>
+                    <CourseCard 
+                        key={item.id}
+                        id={item.id}
+                        title={item.title}
+                        imageUrl={item.imageUrl!}
+                        chaptersLength={item.chapters.length}
+                        price={item.price!}
+                        progress={item.progress}
+                        category={item?.category?.name!}
+                    />
                 ))}
             </div>
             {items.length === 0 && (
