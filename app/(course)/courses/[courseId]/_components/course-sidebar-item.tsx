f4ddbdad82df2b63f114a17343e23cf189d10a5d
Kanwal Arora <kanwal.arora@clinuvel.com>
2024-08-06 14:46:19 +0100

    Single course Page
    Single course page layout , getting all chapters of course

---
+++ app/(course)/courses/[courseId]/_components/course-sidebar-item.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Lock, CheckCircle, PlayCircle } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+
+interface CourseSidebarItemProps {
+    id: string;
+    label: string;
+    isCompleted: boolean;
+    courseId: string;
+    isLocked: boolean;
+}
+
+export const CourseSidebarItem = ({
+    id,
+    label,
+    isCompleted,
+    courseId,
+    isLocked
+}: CourseSidebarItemProps) => {
+
+    const pathname = usePathname();
+    const router = useRouter();
+
+    const Icon = isLocked ? Lock : (isCompleted ? CheckCircle : PlayCircle)
+    const isActive = pathname.includes(id);
+
+    const onClick = () => {
+        router.push(`/courses/${courseId}/chapters/${id}`);
+    };
+
+    return(
+        <button
+            type="button"
+            className={cn(
+                `flex items-center gap-x-2 text-slate-500 text-sm 
+                font-[500] pl-6 transition-all`,
+                isActive && `text-slate-900 font-semibold`,
+                isCompleted && `text-green-500`,
+                isCompleted && isActive && `text-green-200/20`,
+
+            )}
+        >
+            <div className="flex items-center gap-x-2 py-4">
+                <Icon 
+                    size={22}
+                    className={cn(
+                        `text-slate-500`,
+                        isActive && `text-slate-900`,
+                        isCompleted && `text-green-500`,
+                    )}
+                />
+                {label}
+            </div>
+        </button>
+    );
+};
